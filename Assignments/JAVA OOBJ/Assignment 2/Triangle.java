6fd1e319d54e513a04661833a0f155dd5417efbb
Prasad Swami <prasad.swami.cmaug26@gmail.com>
2026-09-11 23:19:52 +0530

    Add files via upload

---
+++ Assignments/JAVA OOBJ/Assignment 2/Triangle.java
@@ -0,0 +1,34 @@
+
+public class Triangle {
+    int a;
+    int b;
+    int c;
+
+
+    Triangle (int a1, int b1, int c1){
+        a = a1;
+        b = b1;
+        c = c1;
+    }
+
+    int perimeter(){
+        int perimeterTri = a + b + c;
+        return perimeterTri;
+    }
+    
+    double area(){
+        double s = (a+b+c)/2;
+        double areaTriangle = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        return areaTriangle;
+        
+    }
+    
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle(3, 4, 5);
+        System.out.println("The Perimeter of triangle is: ");
+        System.out.println(t1.perimeter());
+        System.out.println("The Area of trinagle is: ");
+        System.out.println(t1.area());
+        
+    }
+}
